2a71955b2913eb9c69f57cc90b7a2aa64ef50366
HyunAm <tlfgjawlq@naver.com>
2021-07-18 21:06:36 +0900

    feat : 비밀키 추가

--- backend/movie_project/settings.py
+++ backend/movie_project/settings.py
@@ -22,17 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # 비밀키 관리
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-# reading .env file
-environ.Env.read_env(
-    env_file=os.path.join(BASE_DIR, '.env')
-)
+# env = environ.Env(
+#     # set casting, default value
+#     DEBUG=(bool, False)
+# )
+# # reading .env file
+# environ.Env.read_env(
+#     env_file=os.path.join(BASE_DIR, '.env')
+# )
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'fvi1lfv3v1^_bsh%)x#u=!9z-f81l*!qrxfyhz9edtr=p*sv48'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
