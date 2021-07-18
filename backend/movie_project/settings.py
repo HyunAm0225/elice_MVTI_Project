d06a7678c32569d082a95c63d07adc82f2c08c7b
HyunAm <tlfgjawlq@naver.com>
2021-07-18 21:11:36 +0900

    Feat : django_heroku settings 추가

--- backend/movie_project/settings.py
+++ backend/movie_project/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import environ
 import datetime
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,3 +146,5 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
